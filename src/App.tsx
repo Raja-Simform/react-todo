07cd2d1c0c56a9247b1f599368e0dbe76b06ff97
Raja-Simform <raja.jha@simformsolutions.com>
2025-05-19 17:35:38 +0530

    fix:remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,3 @@ function App() {
 
 export default App;
 
- {/* <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/todos' >
-              <Route index element={<Todos/>}/>
-              <Route path="new" element={<AddTodo/>}/>
-              <Route path=":id" element={<TodoDetail/>}/>
-        </Route>
-        <Route path="*" element={<h2>Page Not Found</h2>}/>
-      </Routes> */}
